5a0e794541f77b24b73f82302b5f049f44934a59
fernandosalomon <fernandosalomon1992@gmail.com>
2024-08-22 19:48:25 -0300

    se corrige la ubicacion de las carpetas pages y routes

---
+++ src/routes/RoutesViews.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import AdministradorPage from "../pages/AdministradorPage";
+import Error404Page from "../pages/Error404Page";
+
+const RoutesViews = () => {
+  return (
+    <>
+      {/* AQUI VA EN NAVBAR */}
+      <Routes>
+        <Route path="/administrador" element={<AdministradorPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<Error404Page />} />
+      </Routes>
+      {/* AQUI VA EN FOOTER */}
+    </>
+  );
+};
+
+export default RoutesViews;
