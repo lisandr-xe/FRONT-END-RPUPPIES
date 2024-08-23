46d80858b495ca0ad8ae706b08b9c71215255e1d
Maxi Soriano <maxi.soriano.70.23@gmail.com>
2024-08-23 20:17:38 -0300

    Cambios en el componente NuestrasMarcas y NuestrosServicios listo para testear

--- src/components/NuestrasMarcas.jsx
+++ src/components/NuestrasMarcas.jsx
@@ -6,6 +6,14 @@ import marca4 from '../assets/img/marca-4.png';
 import marca5 from '../assets/img/marca-5.png';
 import marca6 from '../assets/img/marca-6.png';
 
+const Marca = (props) => {
+    return(
+        <div className={styleNuestrasMarcas.contenedorLogo} data-aos="flip-up">
+            <img className={styleNuestrasMarcas[props.estilo]} src={props.imagen} alt={props.alt}/>
+        </div>
+    );
+}
+
 const NuestrasMarcas = () => {
     return (
         <section className={styleNuestrasMarcas.nuestrasMarcas}>
@@ -13,24 +21,12 @@ const NuestrasMarcas = () => {
                 <h3>Nuestras marcas</h3>
             </article>
             <article className={styleNuestrasMarcas.marcas}>
-                <div className={styleNuestrasMarcas.contenedorLogo} data-aos="flip-up">
-                    <img className={styleNuestrasMarcas.logoRoyalCanin} src={marca1} alt="royal-canin"/>
-                </div>
-                <div className={styleNuestrasMarcas.contenedorLogo} data-aos="flip-up">
-                    <img className={styleNuestrasMarcas.logoPurina} src={marca2} alt="purina"/>
-                </div>
-                <div className={styleNuestrasMarcas.contenedorLogo} data-aos="flip-up">
-                    <img className={styleNuestrasMarcas.logoNexgard} src={marca3} alt="nexgard"/>
-                </div>
-                <div className={styleNuestrasMarcas.contenedorLogo} data-aos="flip-up">
-                    <img className={styleNuestrasMarcas.logoBravexto} src={marca4} alt="bravexto"/>
-                </div>
-                <div className={styleNuestrasMarcas.contenedorLogo} data-aos="flip-up">
-                    <img className={styleNuestrasMarcas.logoPurinaProPlan} src={marca5} alt="purina-pro-plan"/>
-                </div>
-                <div className={styleNuestrasMarcas.contenedorLogo} data-aos="flip-up">
-                    <img className={styleNuestrasMarcas.logoWhiskas} src={marca6} alt="whiskas"/>
-                </div>
+                <Marca key={1} imagen={marca1} alt="royal-canin" estilo="logoRoyalCanin"/>
+                <Marca key={2} imagen={marca2} alt="purina" estilo="logoPurina"/>
+                <Marca key={3} imagen={marca3} alt="nexgard" estilo="logoNexgard"/>
+                <Marca key={4} imagen={marca4} alt="bravexto" estilo="logoBravexto"/>
+                <Marca key={5} imagen={marca5} alt="purina-pro-plan" estilo="logoPurinaProPlan"/>
+                <Marca key={6} imagen={marca6} alt="whiskas" estilo="logoWhiskas"/>
             </article>
         </section>
     );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import NuestrasMarcas from "../components/NuestrasMarcas";
+import NuestrosServicios from "../components/NuestrosServicios";
 
 const HomePage = () => {
     return (
         <>
             <h1>HOME PAGE</h1>
             <NuestrasMarcas/>
+            <NuestrosServicios/>
         </>
     );
 }
