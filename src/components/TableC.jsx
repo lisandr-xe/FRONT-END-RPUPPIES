3b11a67f0a15106509311ba7229beb1e8e23379f
fernandosalomon <fernandosalomon1992@gmail.com>
2024-09-05 19:09:07 -0300

    add functionality to TableC

--- src/components/TableC.jsx
+++ src/components/TableC.jsx
@@ -1,9 +1,64 @@
-import { Container } from "react-bootstrap";
+import DataTable from "datatables.net-react";
+import DT from "datatables.net-dt";
+import { Container, Button } from "react-bootstrap";
+import { useEffect, useRef, useState } from "react";
+DataTable.use(DT);
+import $ from "jquery";
+import "datatables.net-dt/css/dataTables.dataTables.min.css";
+import "datatables.net-responsive-dt/css/responsive.dataTables.min.css";
+import "datatables.net-responsive";
+import "../index.css";
 
-const TableC = () => {
+const TableC = ({ columns, users }) => {
+  const [tableData, setTableData] = useState([]);
+  const tableRef = useRef(null);
+
+  useEffect(() => {
+    setTableData(users);
+
+    const table = $(tableRef.current).DataTable({
+      responsive: true,
+      autoWidth: false,
+      paging: true,
+      rowReorder: {
+        selector: "td:nth-child(2)",
+      },
+    });
+    return () => {
+      if ($.fn.dataTable.isDataTable(tableRef.current)) {
+        table.destroy();
+      }
+    };
+  }, []);
+
   return (
     <>
-      <Container></Container>
+      <Container className="table-responsive">
+        <DataTable className="display nowrap fontPage">
+          <thead>
+            <tr>
+              {columns.map((columnName) => (
+                <th key={columnName}>{columnName}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td className="text-center">{user._id}</td>
+                <td className="text-center">{user.nombre}</td>
+                <td className="text-center">{user.apellido}</td>
+                <td className="text-center">{user.email}</td>
+                <td className="text-center">{user.telefono}</td>
+                <td className="text-center">{user.rol}</td>
+                <td className="text-center ">
+                  <Button className="btnPersonalized3">Editar</Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </DataTable>
+      </Container>
     </>
   );
 };
