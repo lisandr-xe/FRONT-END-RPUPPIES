1e3d97675d834bf6efa768117af84890bff31c27
fernandosalomon <fernandosalomon1992@gmail.com>
2024-09-07 23:16:13 -0300

    continue add functionality to TableC

--- src/components/TableC.jsx
+++ src/components/TableC.jsx
@@ -1,32 +1,88 @@
 import { useEffect, useRef, useState } from "react";
-import { Button, Container, Form } from "react-bootstrap";
+import { Button, Container, Form, Modal } from "react-bootstrap";
 import InputGroup from "react-bootstrap/InputGroup";
 import Swal from "sweetalert2";
 import axios from "axios";
 import DataTable from "react-data-table-component";
-import { width } from "@fortawesome/free-solid-svg-icons/fa0";
+import "../css/TableC.css";
 
 const TableC = ({ tableID }) => {
   const [tableData, setTableData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
   const searchParamBar = useRef(null);
+  const [pending, setPending] = useState(true);
+  const [showEditarPerfil, setShowEditarPerfil] = useState(false);
+  const [modalEditarPerfilData, setModalEditarPerfilData] = useState({});
+
+  const handleCloseEditarPerfil = () => setShowEditarPerfil(false);
+  const handleShowEditarPerfil = (row) => {
+    setShowEditarPerfil(true);
+    setModalEditarPerfilData({
+      nombre: row.nombre,
+      apellido: row.apellido,
+      email: row.email,
+      telefono: row.telefono,
+      rol: row.rol,
+      _id: row._id,
+    });
+  };
 
   const client = axios.create({
     baseURL: "http://localhost:3001/api/usuarios",
   });
 
-  const getUsers = async () => {
-    await client
-      .get("/")
-      .then((res) => {
-        setTableData(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
+  const handleChange = (e) => {
+    setModalEditarPerfilData({
+      ...modalEditarPerfilData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    console.log(modalEditarPerfilData, modalEditarPerfilData._id);
+
+    try {
+      const response = await client.put(
+        `/${modalEditarPerfilData._id}`,
+        modalEditarPerfilData
+      );
+      if (response.status === 200) {
+        Swal.fire({
+          title: "Los datos se actualizaron con exito",
+          text: "Los datos del usuario se actualizaron correctamente",
+          icon: "success",
+        });
+      } else {
+        Swal.fire({
+          title: "Hubo un problema",
+          text: `Error ${response.status}: No se pudieron actualizar los datos del usuario`,
+          icon: "error",
+        });
+      }
+    } catch (error) {
+      Swal.fire({
+        title: "Hubo un problema",
+        text: `${error}`,
+        icon: "error",
       });
+    }
+  };
+
+  const getUsers = async () => {
+    try {
+      const response = await client.get("/");
+      await setTableData(response.data);
+      await setPending(false);
+      await setFilteredData(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
-    !tableData.length && getUsers();
+    getUsers();
   }, []);
 
   const deshabilitarUsuario = async (userID, is_bloqueado) => {
@@ -84,7 +140,12 @@ const TableC = ({ tableID }) => {
     {
       name: "Nombre",
       value: "nombre",
-      selector: (row) => `${row.nombre} ${row.apellido}`,
+      selector: (row) => row.nombre,
+    },
+    {
+      name: "Apellido",
+      value: "apellido",
+      selector: (row) => row.apellido,
     },
     {
       name: "Email",
@@ -107,23 +168,35 @@ const TableC = ({ tableID }) => {
       selector: (row) => (
         <>
           <div className="d-flex gap-2">
-            <Button className="btnPersonalized3">Editar</Button>
-            <Button
+            <button
+              className="btnPersonalized3"
+              onClick={() => {
+                handleShowEditarPerfil(row);
+              }}
+            >
+              <i className="bi bi-pencil-square fs-4"></i>
+            </button>
+
+            <button
               className="btn btn-warning"
               onClick={() => {
                 deshabilitarUsuario(row._id, row.bloqueado);
               }}
             >
-              {row.bloqueado ? "Habilitar" : "Deshabilitar"}
-            </Button>
-            <Button
+              {row.bloqueado ? (
+                <i className="bi bi-person-fill-slash fs-3"></i>
+              ) : (
+                <i className="bi bi-person-fill-check fs-3"></i>
+              )}
+            </button>
+            <button
               className="btn btn-danger"
               onClick={() => {
                 eliminarUsuario(row._id);
               }}
             >
-              Eliminar
-            </Button>
+              <i className="bi bi-trash fs-4"></i>
+            </button>
           </div>
         </>
       ),
@@ -134,48 +207,33 @@ const TableC = ({ tableID }) => {
   const customSearch = (obj, filter, filterValue) => {
     const newObj = [];
 
-    if (Array.isArray(filter)) {
-      Object.keys(obj).forEach((key) => {
-        var is_added = false;
-        filter.forEach((filterKey) => {
-          if (
-            !is_added &&
-            obj[key][filterKey]
-              .toLowerCase()
-              .indexOf(filterValue.toLowerCase()) >= 0
-          ) {
-            newObj.push(obj[key]);
-            is_added = true;
-          }
-        });
-      });
-      return newObj;
-    } else {
-      Object.keys(obj).forEach((key) => {
-        obj[key][filter].toLowerCase().indexOf(filterValue.toLowerCase()) >=
-          0 && newObj.push(obj[key]);
-      });
-      return newObj;
-    }
+    Object.keys(obj).forEach((key) => {
+      obj[key][filter].toLowerCase().indexOf(filterValue.toLowerCase()) >= 0 &&
+        newObj.push(key);
+    });
+    return newObj;
   };
 
   const handleSearch = (e) => {
     let newTableData = [];
     if (e.target.value === "") {
-      getUsers();
-    }
-    const searchParam = searchParamBar.current.value;
-
-    if (searchParam === "all") {
-      newTableData = customSearch(
-        tableData,
-        ["_id", "nombre", "email", "telefono", "rol"],
-        e.target.value
-      );
-      setTableData(newTableData);
+      setFilteredData(tableData);
     } else {
+      const searchParam = searchParamBar.current.value;
       newTableData = customSearch(tableData, searchParam, e.target.value);
-      setTableData(newTableData);
+      handleFilterData(newTableData);
+    }
+  };
+
+  const handleFilterData = (dataToShow) => {
+    if (dataToShow.length) {
+      let newData = [];
+      dataToShow.forEach((value) => {
+        newData.push(tableData[value]);
+      });
+      setFilteredData(newData);
+    } else {
+      setFilteredData([]);
     }
   };
 
@@ -196,7 +254,6 @@ const TableC = ({ tableID }) => {
               aria-label="Default select example"
               ref={searchParamBar}
             >
-              <option value={"all"}>Todos los campos</option>
               {columns.map(
                 (column) =>
                   column.name !== "Opciones" && (
@@ -208,8 +265,145 @@ const TableC = ({ tableID }) => {
             </Form.Select>
           </InputGroup>
         </Form>
-        <DataTable columns={columns} data={tableData} pagination />
+        <div className="table--wrapper">
+          <DataTable
+            columns={columns}
+            data={filteredData}
+            pagination
+            fixedHeader
+            responsive
+            paginationComponentOptions={{
+              rowsPerPageText: "Filas por página",
+              rangeSeparatorText: "de",
+              selectAllRowsItem: true,
+              selectAllRowsItemText: "Todos",
+            }}
+            progressPending={pending}
+          />
+        </div>
       </Container>
+      <Modal show={showEditarPerfil} onHide={handleCloseEditarPerfil} centered>
+        <Modal.Header
+          closeButton
+          closeVariant="white"
+          className="bgColorPrincipal"
+        >
+          <Modal.Title className="text-white">Editar Perfil</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="bgColorFondo">
+          <Form id="form-editar-perfil">
+            <div className="d-flex gap-3">
+              <Form.Group className="mb-3">
+                <Form.Label htmlFor="nombreEditar" className="fw-bolder">
+                  Nombre
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  id="nombreEditar"
+                  minLength="3"
+                  maxLength="25"
+                  name="nombre"
+                  value={modalEditarPerfilData.nombre}
+                  onChange={handleChange}
+                  required
+                  className="bgInput"
+                />
+                <div id="nombreErrorEditar"></div>
+              </Form.Group>
+
+              <Form.Group className="mb-3">
+                <Form.Label htmlFor="apellidoEditar" className="fw-bolder">
+                  Apellido
+                </Form.Label>
+                <Form.Control
+                  type="text"
+                  id="apellidoEditar"
+                  minLength="3"
+                  maxLength="25"
+                  name="apellido"
+                  value={modalEditarPerfilData.apellido}
+                  onChange={handleChange}
+                  required
+                  className="bgInput"
+                />
+                <div id="apellidoErrorEditar"></div>
+              </Form.Group>
+            </div>
+
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="emailEditar" className="fw-bolder">
+                Email
+              </Form.Label>
+              <Form.Control
+                type="email"
+                id="emailEditar"
+                minLength="8"
+                maxLength="20"
+                name="email"
+                value={modalEditarPerfilData.email}
+                onChange={handleChange}
+                required
+                className="bgInput"
+                aria-describedby="emailHelp"
+              />
+              <div id="emailErrorEditar"></div>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="telefonoEditar" className="fw-bolder">
+                Teléfono
+              </Form.Label>
+              <Form.Control
+                type="tel"
+                id="telefonoEditar"
+                placeholder="Ingresa tu teléfono"
+                maxLength="10"
+                pattern="\d{10}"
+                name="Teléfono"
+                value={modalEditarPerfilData.telefono}
+                onChange={handleChange}
+                required
+                className="bgInput"
+              />
+              <div id="telefonoErrorEditar"></div>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label className="fw-bolder">Rol</Form.Label>
+              <Form.Select
+                aria-label="Default select example"
+                className="bgInput"
+                onChange={handleChange}
+                value={modalEditarPerfilData.rol}
+                name="rol"
+              >
+                <option value="user">user</option>
+                <option value="admin">admin</option>
+              </Form.Select>
+            </Form.Group>
+
+            <div className="d-flex align-items-center justify-content-center">
+              <Button
+                type="submit"
+                id="botonGuardarCambios"
+                className={`$"btnPersonalized2} mx-1 fw-bold`}
+                aria-label="Guardar cambios"
+                onClick={handleClick}
+              >
+                Guardar cambios
+              </Button>
+              <Button
+                type="button"
+                className={`$"btnPersonalized1} mx-1 fw-bold`}
+                aria-label="Cancelar"
+                onClick={handleCloseEditarPerfil}
+              >
+                Cancelar
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </>
   );
 };
--- src/pages/AdministradorUsuarios.jsx
+++ src/pages/AdministradorUsuarios.jsx
@@ -3,39 +3,9 @@ import TableC from "../components/TableC";
 import axios from "axios";
 
 const AdministradorUsuarios = () => {
-  const [usuarios, setUsuarios] = useState();
-
-  const client = axios.create({
-    baseURL: "http://localhost:3001/api/usuarios",
-  });
-
-  useEffect(() => {
-    client
-      .get("/")
-      .then((res) => {
-        setUsuarios(res.data);
-        console.log(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
   return (
     <>
-      <TableC
-        tableID={"usuarios"}
-        columns={[
-          "ID",
-          "Nombre",
-          "Apellido",
-          "Email",
-          "Teléfono",
-          "Rol",
-          "Opciones",
-        ]}
-        tdata={usuarios}
-      />
+      <TableC tableID={"usuarios"} />
     </>
   );
 };
