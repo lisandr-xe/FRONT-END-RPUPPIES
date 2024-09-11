aeded82230a75cae751380fcd39f19647892687b
amc961 <gonzalomainardi@gmail.com>
2024-09-11 02:30:58 -0300

    se comitean ultimos cambios

--- src/pages/AdministradorTurnos.jsx
+++ src/pages/AdministradorTurnos.jsx
@@ -15,20 +15,7 @@ const AdministradorTurnos = () => {
       
       const turnosData = turnosResponse.data;
       const mascotasData = mascotasResponse.data;
-      
-
-            function formatDateToYYYYMMDD(date) {
-   
-              const fecha = new Date (date)
-              const year = fecha.getFullYear();
-              const month = String(fecha.getMonth() + 1).padStart(2, '0');
-              const day = String(fecha.getDate()).padStart(2, '0');
-              return `${year}-${month}-${day}`;
-            }
-
-              
-
-
+                
       const mapaMascotas = crearMapaMascotas(mascotasData);
 
 
